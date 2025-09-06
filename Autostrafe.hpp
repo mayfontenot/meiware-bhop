9b87358247e5791a3312328ee41d82f65c78d95f
May <92554133+mayfontenot@users.noreply.github.com>
2025-09-05 19:09:28 -0500

    Add files via upload

--- Autostrafe.hpp
+++ Autostrafe.hpp
@@ -12,10 +12,7 @@ void AutostrafeThread()
 		if (!conBools[CON_B_AUTOSTRAFE] || !GetAsyncKeyState(VK_XBUTTON2)) //if not active, then skip iteration and reset movement (only once)
 		{
 			if (runOnce)
-			{
-				*(int*)(client + forceLeft) = *(int*)(client + forceRight) = 0;
-				runOnce = lastDir = false;
-			}
+				runOnce = lastDir = *(bool*)(client + forceLeft) = *(bool*)(client + forceRight) = false;
 
 			continue;
 		}
@@ -23,16 +20,10 @@ void AutostrafeThread()
 		GetWindowRect(GetForegroundWindow(), &screen);
 		GetCursorPos(&cursor);
 
-		if (cursor.x < screen.right / 2 || cursor.x == screen.right / 2 && !lastDir) //continue strafing towards a direction even if the mouse is not moving
-		{
-			*(int*)(client + forceLeft) = (*(int*)(client + forceRight))--;
-			runOnce = !(lastDir = false); //sets runOnce to true and lastDir to false (left)
-		}
-		else if (cursor.x > screen.right / 2 || cursor.x == screen.right / 2 && lastDir)
-		{
-			*(int*)(client + forceRight) = (*(int*)(client + forceLeft))--;
-			runOnce = lastDir = true;
-		}
+		if (cursor.x < screen.right / 2)
+			runOnce = !(lastDir = !(*(bool*)(client + forceLeft) = !(*(bool*)(client + forceRight) = false)));
+		else if (cursor.x > screen.right / 2)
+			runOnce = lastDir = *(bool*)(client + forceRight) = !(*(bool*)(client + forceLeft) = false);
 
 		Sleep(1);
 	}
